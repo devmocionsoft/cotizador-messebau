1a2a4de25c51a1edd53d4da5d73c3acc529965ce
Diego Rojas <diegorojas431@gmail.com>
2022-06-02 09:03:48 -0500

    Corrigiendo CORS

--- src/Test3D.jsx
+++ src/Test3D.jsx
@@ -18,10 +18,27 @@ const CameraController = () => {
   return null;
 };
 
-const objUrl = "https://firebasestorage.googleapis.com/v0/b/messebau-121f2.appspot.com/o/Room.obj?alt=media&token=ef2c9147-73d6-4252-a921-86e31422b1cc";
+// const objUrl = "https://firebasestorage.googleapis.com/v0/b/messebau-121f2.appspot.com/o/Room.obj?alt=media&token=ef2c9147-73d6-4252-a921-86e31422b1cc";
 
-function Scene() {
-  const obj = useLoader(OBJLoader, objUrl);
+const objUrl = async () => {
+  const url = await fetch(`https://firebasestorage.googleapis.com/v0/b/messebau-121f2.appspot.com/o/Room.obj?alt=media&token=ef2c9147-73d6-4252-a921-86e31422b1cc`, {
+    method : "GET",
+    mode: 'cors',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    }
+  });
+
+  console.log(url);
+
+  return url;
+}
+
+
+async function Scene () {
+
+  const obj = await useLoader(OBJLoader, objUrl);
+  // console.log( obj );
   return <primitive object={obj} />;
 }
 
