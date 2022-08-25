a53dac8d4b6bcccc3fe31d1d95afdcc28fe0c1fe
Jeisson <jeissonssp@gmail.com>
2022-08-25 17:00:22 -0500

    Secciones principales

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <section className="section_1" id="section_1"></section>
+      <section className="section_2" id="section_2"></section>
+      <section className="section_3" id="section_3"></section>
+      <section className="section_4" id="section_4"></section>
+      <section className="section_5" id="section_5"></section>
+    </div>
+  );
 }
