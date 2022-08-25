bd9964a0203d98c72424de6eb77c5dff6ee184f2
Jeisson <jeissonssp@gmail.com>
2022-08-25 16:54:49 -0500

    redirects

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
 import Model3D from "./screens/Model3D";
 
 export default function MainRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Model3D />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/cotizador" element={<Model3D />} />
     </Routes>
   );
 }
